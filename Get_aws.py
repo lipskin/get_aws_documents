10c7b99fc967a80e0e07d8f1b6b1f183a5e3bc1e
yiddu <yiddu@thoughtworks.com>
2016-09-20 17:36:37 +0800

    build seccuess

--- Get_aws.py
+++ Get_aws.py
@@ -19,17 +19,24 @@ def get_document():
         if not os.path.exists(service_name):
             os.mkdir(service_name)
         os.chdir(service_name)
-        services_tag = document_soup.find('h4',id=service_name).parent.parent.parent.parent.find_all('a')[1:-1]
+        services_tag = document_soup.find('h4',id=service_name.replace(' ', '_')).parent.parent.parent.next_sibling.next_sibling.find_all('a')
         for service_subname in services_tag:
-            if not os.path.exists(service_subname.text):
-                os.mkdir(service_subname.text)
-            os.chdir(service_subname.text)
-            service_url = urllib.parse.urljoin('https://aws.amazon.com', service_subname['href'])
-            service_page = requests.get(service_url).text
-            service_soup = BeautifulSoup(service_page, 'lxml')
-            pdf_urls = [ x['href'] for x in service_soup.find_all('a',text='PDF')]
-            for pdf_url in pdf_urls:
-                os.system('curl -O ' + pdf_url + ' &')
+            service_subname_folder = service_subname.text.replace('/', ':')
+            if not os.path.exists(service_subname_folder):
+                os.mkdir(service_subname_folder)
+            os.chdir(service_subname_folder)
+            try:
+                service_url = urllib.parse.urljoin('https://aws.amazon.com', service_subname['href'])
+                service_page = requests.get(service_url).text
+                service_soup = BeautifulSoup(service_page, 'lxml')
+                pdf_urls = [ x['href'] for x in service_soup.find_all('a',text='PDF')]
+                for pdf_url in pdf_urls:
+                    try:
+                        os.system('curl -O ' + pdf_url + ' &')
+                    except:
+                        pass
+            except:
+                pass
             os.chdir('..')
         os.chdir('..')
     os.chdir('..')
